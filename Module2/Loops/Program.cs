dc3e2719328c2a07d5aacb33a10a8f325f594a46
Mansi Degda <87441108+mansidegda1612@users.noreply.github.com>
2021-10-14 00:29:08 +0530

    Add files via upload

---
+++ Module2/Loops/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // for loop
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            // foreach
+            int[] array = new int[] { 1, 2, 3, 4, 5};
+            foreach(int item in array)
+            {
+                Console.WriteLine(item);
+            }
+
+            // while
+            int num1 = 5;
+            while (num1 < 10)
+            {
+                Console.WriteLine(num1);
+                num1++;
+            }
+
+            // do..while
+            num1 = 5;
+            do
+            {
+                Console.WriteLine(num1);
+                num1++;
+            }
+            while (num1 < 5);
+
+            //break
+            for (int i = 1; i <= 5; i++)
+            {
+                if (i == 3)
+                    break;
+                Console.WriteLine(i);
+            }
+
+            //continue
+            for (int i = 1; i <= 5; i++)
+            {
+                if (i == 3)
+                    continue;
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
