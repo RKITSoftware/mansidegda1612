2877887bec5f550bf0c74156b40fb700d4345a25
Mansi Degda <87441108+mansidegda1612@users.noreply.github.com>
2021-10-14 00:21:32 +0530

    Add files via upload

---
+++ Module2/Arrays/Arrays/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[5]; //n is an array of 10 integers 
+       
+            // initialize elements  
+            for (int i = 0; i < 5; i++)
+            {
+                array[i] = i + 10;
+            }
+
+            //   print array element value 
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Element[{0}] = {1}", i, array[i]);
+            }
+           
+        }
+    }
+}
