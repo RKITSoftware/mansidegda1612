9879ed0179dfd3aed314ebd7437a1666d949800f
Mansi Degda <87441108+mansidegda1612@users.noreply.github.com>
2021-11-15 15:20:23 +0530

    Interface & inheritance

---
+++ Module4/Interface & inheritance/Inheritance/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+
+namespace Inheritance
+{
+    //Single inheritance  
+    //base class 
+    public class Animal    
+    {
+        public void Name()
+        {
+            Console.WriteLine( "I am Animal");
+        }
+    }
+    //derived class  
+    public class Dog : Animal   
+    {
+        //derived class method   
+        public void breed()
+        {
+            Console.WriteLine("I am Husky");
+        }
+    }
+
+    //Hierarchical inheritance
+    //base class 
+    class A     
+    {
+        public void msg()
+        {
+            Console.WriteLine("this is A class ");
+        }
+    }
+    //derived class 1
+    class B : A
+    {
+        public void info()
+        {
+            msg();//method of base class 
+            Console.WriteLine("this is B class ");
+        }
+    }
+    //derived class 2
+    class C : A
+    {
+        public void getinfo()
+        {
+            msg();
+            Console.WriteLine("this is c class ");
+        }
+    }
+
+    //Multilevel inheritance
+    //base class 
+    public class Asia
+    {
+        public void continentname()
+        {
+            Console.WriteLine("this is the Asia class");
+        }
+    }
+    //India inherit Asia
+    public class India : Asia
+    {
+        public void countryname()
+        {
+            continentname();
+            Console.WriteLine("this is the India Class");
+        }
+    }
+    //Gujrat inherit India
+    public class Gujrat : India
+    {
+        public void statename()
+        {
+            continentname();
+            countryname();
+            Console.WriteLine("this is the Gujrat Class");
+        }
+    }
+
+    //Multiple inheritance 
+    public interface Parent1 //ineterface  1    
+    {
+        void eyecolor(string color);
+    }
+    public interface Parent2  //Interface 2    
+    {
+        void skincolor(string color);
+    }
+    public class Child : Parent1, Parent2 //implementatin    
+    {
+        public void eyecolor(string color)
+        {
+            Console.WriteLine("Child eye color is" + color);
+        }
+        public void skincolor(string color)
+        {
+            Console.WriteLine("Child skin color is" + color);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Single inheritance  
+            //object of Dog(derived) class
+            Dog objDog = new Dog();
+            //method of base class
+            objDog.Name();
+            objDog.breed();
+
+            //Hierarchical inheritance
+            B objB = new B();
+            objB.info();
+
+            C objC = new C();
+            objC.getinfo();
+
+            //Multilevel inheritance
+            Asia objAsia = new Asia();
+            India objIndia = new India();
+            Gujrat objGujrat = new Gujrat();
+
+            objAsia.continentname();
+            objIndia.countryname();
+            objGujrat.statename();
+
+            //Multiple inheritance 
+            Child objChild = new Child();
+            objChild.eyecolor("blue");
+            objChild.eyecolor("brown");
+
+        }
+    }
+}
---
+++ Module4/Interface & inheritance/Interface/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace Interface
+{
+    //interface
+    public interface myInterface1
+    {
+        // interface members
+        void getdata(string name,int age);
+        void printdata();
+    }
+
+    public interface myInterface2
+    {
+        // interface members
+        void getCompny(string company);
+        void printCompny();
+    }
+
+    //class implement interface
+    public class Student : myInterface1
+    {
+        string name;
+        int age;
+
+        //implementation of interface members
+        //public getdata method of interface
+        public void getdata(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+        //public printdata method of interface
+        public void printdata()
+        {
+            Console.WriteLine("Name:" + this.name);
+            Console.WriteLine("Age:" + this.age);
+        }
+    }
+    //Class Employee implement two interfaces 
+    public class Employee : myInterface1, myInterface2
+    {
+        string name;
+        int age;
+        string company;
+
+        //implementation of myInterface1 members
+
+       
+        public void getdata(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+        public void printdata()
+        {
+            Console.WriteLine("Name:" + this.name);
+            Console.WriteLine("Age:" + this.age);
+            Console.WriteLine("Company:" + this.company);
+        }
+
+        //implementation of myInterface2 members
+        public void getCompny(string company)
+        {
+            this.company = company;
+        }
+        //Explicit implementation
+        void myInterface2.printCompny()
+        {
+            Console.WriteLine("Company:" + this.company);
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //object of class student
+            Student objStudent = new Student();
+            Console.WriteLine("Student");
+            objStudent.getdata("Mansi",20);
+            objStudent.printdata();
+
+            //object of class Employee
+            Employee objEmployee = new Employee();
+            Console.WriteLine("Employee");
+            objEmployee.getdata("Mansi",20);
+            objEmployee.getCompny("RKIT");
+            objEmployee.printdata();
+            //error
+            //objEmployee.printCompny();
+        }
+    }
+}
+
