dd46ee9193695b3abedb8249b28a41ac7c1e08e5
Mansi Degda <87441108+mansidegda1612@users.noreply.github.com>
2021-11-27 22:19:34 +0530

    Serialization Demo

---
+++ Module5/Serialization/Models/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Serialization.Models
+{
+    public class Book
+    {
+        public int BookID { get; set;}
+        public string BookName{ get; set; }
+        public string AuthorName { get; set; }
+
+    }
+}
