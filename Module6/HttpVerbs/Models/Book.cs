3a63ec162ad2fbcb0bf1ae59129ced72c313cca2
Mansi Degda <87441108+mansidegda1612@users.noreply.github.com>
2022-01-05 11:06:54 +0530

    Update Book.cs

--- Module6/HttpVerbs/Models/Book.cs
+++ Module6/HttpVerbs/Models/Book.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,11 +6,20 @@ using System.ComponentModel.DataAnnotations;
 
 namespace HttpVerbs.Models
 {
+    /// <summary>
+    /// Book model
+    /// fields :
+    /// public BookID
+    /// public BookName
+    /// public AuthorName
+    /// </summary>
     public class Book
     {
         [Key]
         public int BookID { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "This property is Required")]
+        [StringLength(50,ErrorMessage ="string length can't be more than 50")]
         public string BookName { get; set; }
         public string AuthorName { get; set; }
     }
